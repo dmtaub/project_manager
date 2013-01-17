28c93c05fed8c05d866c64cc20d218d41683dc49
Dan Taub <dmtaub@gmail.com>
2013-01-17 13:35:37 -0500

    admin login required

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,5 +1,5 @@
 class AdminController < ApplicationController
-  before_filter :authenticate_user!
+  before_filter :verify_admin
   
   def index
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  def verify_admin
+    authenticate_user!
+    if !current_user || !current_user.admin?
+      flash[:notice] = "Need Administrator rights to Access that page."
+      redirect_to root_path
+    end
+  end
   protect_from_forgery with: :exception
 end

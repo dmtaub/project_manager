5d1bcc7746b278708a09f00d2d1c4e118351c6b2
Dan Taub <dmtaub@gmail.com>
2013-01-17 11:27:18 -0500

    adding files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ProjectManager::Application.config.secret_key_base = '5c3f1ea5bdf07beaf7957060e9f06ff9b28c4fd04d757ede883e3dc3dd83fe8c18b520ae0cc82375deb44558b90fefcc5e7ad5467944a17545fa12ea52b529ac'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ProjectManager::Application.config.session_store :encrypted_cookie_store, key: '_project_manager_session'

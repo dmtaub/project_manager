be86023d4d6609f34b194a957ed5855a6b300f09
Dan Taub <dmtaub@gmail.com>
2013-01-30 16:13:59 -0500

    fixed layout for alert message, added status selection option

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,6 +2,9 @@ class Project < ActiveRecord::Base
   belongs_to :user
   validates_presence_of :user_id
 
+  def self.statuses
+    ["Not Started", "In Progress", "Paused", "Finished"]
+  end
   def self.displayColumns
     ["project","target_completion","status","notes"]
   end
